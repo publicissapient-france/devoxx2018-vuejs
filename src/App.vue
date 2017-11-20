28519e8f02f3765d17491a3d5a837f2e275087d1
tchampion <tchampion@xebia.fr>
2017-11-20 19:13:43 +0100

    pre-requis step 1

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,85 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header>
+      <h1>SuperCook</h1>
+    </header>
+
+    <!-- TODO afficher la recette -->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  // TODO importer le service
+  // TODO import le composant Recipe
+
+  // TODO définir le composant
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+    header {
+      text-align: center;
+
+      h1 {
+        font-family: 'Cookie', cursive;
+        font-size: 5em;
+        text-shadow: 2px 8px 2px #aca2a2;
+        margin: 30px 0;
+      }
+
+      .menu {
+        display: inline-block;
+
+        ul {
+          display: flex;
+          margin: 0;
+          padding: 0;
+
+          li {
+            list-style-type: none;
+
+            margin: 0 8px;
+            border-bottom: 1px solid;
+            border-radius: 11px;
+
+            a {
+              text-decoration: none;
+              color: rgb(108, 108, 108);
+              padding: 7px 30px;
+              display: inline-block;
+            }
+
+            &:hover {
+              background: #f6f6f6;
+            }
+          }
+        }
+      }
+    }
+
+    main {
+      .content {
+        width: 90%;
+        margin: auto;
+
+        .title {
+          box-shadow: 0 8px 13px 0 #aeadad;
+          padding: 0 11px;
+          border-radius: 0 0 8px 8px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          height: 30px;
+          margin-bottom: 10px;
+          margin-top: 10px;
+        }
+      }
+    }
+
+    footer {
+      height: 20px;
+    }
+  }
 </style>
---
+++ src/components/Recipe.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="recipe">
+    <h3>
+      <!-- TODO insérer le nom -->
+    </h3>
+    <div class="details">
+      <div class="info">
+        <!--- TODO insérer l'image --->
+        <div><!-- TODO insérer le temps de cuisson --></div>
+      </div>
+      <div class="story">
+        <p class="description"><!-- TODO insérer l'histoire --></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // TODO écrire le code du composant
+</script>
+
+<style scoped lang="scss">
+  .recipe {
+    background: #eee;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+    box-shadow: 0 8px 13px 0 #aeadad;
+
+    .details {
+      display: flex;
+
+      .info {
+        text-align: center;
+        margin-right: 10px;
+        img {
+          width: 160px;
+        }
+      }
+
+      .story {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        text-align: justify;
+
+        .description {
+          flex: 1;
+          margin: 0;
+        }
+
+        .favorite {
+          align-self: flex-end;
+        }
+      }
+    }
+  }
+</style>
